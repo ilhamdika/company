de8b33f25cf096a5e895bab2b21f6d3c378f2893
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2024-01-13 14:53:55 +0700

    fix: UI position

--- resources/js/Pages/User/Index.jsx
+++ resources/js/Pages/User/Index.jsx
@@ -59,7 +59,7 @@ export default function Index({landing ,berita, artikel}) {
                         <h1 className="font-semibold text-4xl dark:text-white" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500">{landing.motto_title}</h1>
                     </div>
                 </div>
-                
+                <div className="mobile: mt-10 mt-10">
                 <div className="grid mobile:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-4 dekstop:grid-cols-4 gap-4" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
                 {artikel.map((artikel, i=1) => (
                     <Card
@@ -71,6 +71,7 @@ export default function Index({landing ,berita, artikel}) {
                     href={route('user.artikel_detail', artikel.slug)}
                     />
                 ))}
+                </div>
                 </div>
                 <div className="flex justify-center">
                     <Link className="bg-blue-600 mt-10 border-b w-40 justify-center h-10 rounded-3xl inline-flex items-center hover:border-b-2 hover:border-blue-500" href={route('user.artikel')}>Lihat semua</Link>
@@ -85,28 +86,30 @@ export default function Index({landing ,berita, artikel}) {
                         <Link className="bg-blue-600 mt-10 border-b w-40 justify-center h-10 rounded-xl inline-flex items-center hover:border-b-2 hover:border-blue-500" href={route('user.tentang-kami')}>Lebih lanjut</Link>
                     </div>
                 </div>
+
+                <div className="mt-5">
+                    <h1 className="font-semibold text-3xl dark:text-white">Apa yang baru dari Dakara?</h1>
+                    <p className="font-light dark:text-white mt-3">Cari tahu lebih lanjut tentang aktivitas kami.</p>
+
+                    <div className="grid mobile:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-4 dekstop:grid-cols-4 gap-4" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
+                    {berita.map((berita, i=1) => (
+                        <Card
+                        key={berita.id}
+                        thumbnail={`/storage/${berita.image}`}
+                        title={berita.title.split(' ').slice(0, 5).join(' ')}
+                        date={format(new Date(berita.date), 'dd MMMM yyyy')}
+                        description={berita.content.split(' ').slice(0, 10).join(' ')}
+                        href={route('user.artikel_detail', berita.slug)}
+                        />
+                    ))}
+                    </div>
+                    <div className="flex justify-center mb-10">
+                        <Link className="bg-blue-600 mt-10 border-b w-40 justify-center h-10 rounded-3xl inline-flex items-center hover:border-b-2 hover:border-blue-500" href={route('user.berita')}>Lihat semua</Link>
+                    </div>
+                </div>
             </div>
 
-            <div className="mt-5">
-                <h1 className="font-semibold text-3xl dark:text-white">Apa yang baru dari Dakara?</h1>
-                <p className="font-light dark:text-white mt-3">Cari tahu lebih lanjut tentang aktivitas kami.</p>
-
-                <div className="grid mobile:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-4 dekstop:grid-cols-4 gap-4" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
-                {berita.map((berita, i=1) => (
-                    <Card
-                    key={berita.id}
-                    thumbnail={`/storage/${berita.image}`}
-                    title={berita.title.split(' ').slice(0, 5).join(' ')}
-                    date={format(new Date(berita.date), 'dd MMMM yyyy')}
-                    description={berita.content.split(' ').slice(0, 10).join(' ')}
-                    href={route('user.artikel_detail', berita.slug)}
-                    />
-                ))}
-                </div>
-                <div className="flex justify-center mb-10">
-                    <Link className="bg-blue-600 mt-10 border-b w-40 justify-center h-10 rounded-3xl inline-flex items-center hover:border-b-2 hover:border-blue-500" href={route('user.berita')}>Lihat semua</Link>
-                </div>
-            </div>
+            
             
         </LayoutPage>
     
