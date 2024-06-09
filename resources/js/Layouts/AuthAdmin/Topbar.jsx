3081176ade7bde9c6ff04724406b851c8a46c759
ilhamdika <ilham27dika@gmail.com>
2024-06-09 14:29:50 +0700

    fix: change icon logout

--- resources/js/Layouts/AuthAdmin/Topbar.jsx
+++ resources/js/Layouts/AuthAdmin/Topbar.jsx
@@ -6,6 +6,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import { CiSettings } from "react-icons/ci";
 // import { Link } from '@inertiajs/react';
 
 
@@ -26,7 +27,7 @@ export default function Topbar({header}) {
 
 
     return (
-        <div className="bg-gray-200">
+        <div className="border-2">
             <nav className=" dark:bg-slate-600 border-b border-gray-100">
                 <div className="max-w-7xl mx-auto px-4 laptop:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
@@ -52,21 +53,9 @@ export default function Topbar({header}) {
                                         <span className="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                className="inline-flex items-center px-3 py-2 border border-transparent text-xl leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                                             >
-                                                
-                                                <svg
-                                                    className="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
+                                            <CiSettings />
                                             </button>
                                         </span>
                                     </Dropdown.Trigger>
