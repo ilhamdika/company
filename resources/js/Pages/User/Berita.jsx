e77e13749e2391772025eb09bbb5a205812363bd
ilhamdika <ilham27dika@gmail.com>
2024-06-09 14:19:21 +0700

    feat: paginate news

--- resources/js/Pages/User/Berita.jsx
+++ resources/js/Pages/User/Berita.jsx
@@ -2,33 +2,18 @@ import LayoutPage from "@/Layouts/AuthUser/LayoutPage";
 import Card from "@/Components/Card";
 import { Head } from "@inertiajs/react";
 import { format } from "date-fns";
+import { Link } from "@inertiajs/react";
 
-export default function Berita({beritas}) {
-    // console.log(beritas)
-
-    // const fullText = beritas.title;
-
-    // const words = fullText.split(' ');
-
-    // // Menampilkan hanya sepuluh kata pertama
-    // const displayedText = words.slice(0, 2).join(' ');
+export default function Berita({beritas, firstPageUrl, lastPageUrl}) {
+   
     return (
         <LayoutPage>
             <Head title="Berita" />
             <div className="mobile:pt-20">
                 <h1 className="font-semibold text-4xl flex justify-center items-center dark:text-white">Berita</h1>
                 <div className="grid grid-cols-4 laptop:flex-row dekstop:flex-row mobile:grid-cols-1 rounded-xl mobile:h-auto mt-10">
-                    {/* {[1,2,3,4].map(i=> (
-                    <Card
-                    key={i}
-                    thumbnail={'https://i.pinimg.com/564x/bb/a6/67/bba667e0370042ca691fd5c08bfd14ca.jpg'}
-                    title={'Web Developer'}
-                    description={'Create a website for information, business etc., which can be accessed digitally'}
-                    href={'https://www.google.com'}
-                    />
-                    ))} */}
 
-                    {beritas.map((berita)=> (
+                    {beritas.data.map((berita)=> (
                         <Card
                         key={berita.id}
                         thumbnail={`/storage/${berita.image}`}
@@ -38,6 +23,29 @@ export default function Berita({beritas}) {
                         href={route('user.berita_detail', berita.slug)}
                         />
                     ))}
+                </div>
+                <div className="pagination-links flex justify-end mt-4 space-x-1 text-sm">
+                    <Link
+                        href={firstPageUrl}
+                        className="px-2 py-1 rounded bg-white hover:bg-gray-200"
+                    >
+                        First
+                    </Link>
+                    {beritas.links.map((link, index) => (
+                        <Link
+                            key={index}
+                            href={link.url}
+                            className={`px-2 py-1 ${link.active ? 'bg-emerald-300 text-white border-emerald-500 rounded-xl' : 'bg-white border-gray-300 hover:bg-gray-200'}`}
+                        >
+                            {link.label === '&laquo; Previous' ? '<' : link.label === 'Next &raquo;' ? '>' : link.label}
+                        </Link>
+                    ))}
+                    <Link
+                        href={lastPageUrl}
+                        className="px-2 py-1 rounded bg-white border-gray-300 hover:bg-gray-200"
+                    >
+                        Last
+                    </Link>
                 </div>
             </div>
         </LayoutPage>
